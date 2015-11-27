1fda594fbeaef5f10661c6e0148c289232baacaa
Trevor Coehoorn <tcoehoorn@gmail.com>
2015-11-27 08:39:41 -0700

    Documented angular javascript/correct add task display

--- public/js/index.js
+++ public/js/index.js
@@ -1,42 +1,50 @@
+/*
+ * Task Tracker
+ */
 
 var taskApp = angular.module('taskApp', []);
 
 taskApp.controller('TaskController', ['$scope', '$http', function($scope, $http) {
     $scope.formData = {};
 
-    $scope.processForm = function() {
+    /*
+     * Save task data
+     */
+    $scope.saveTask = function() {
         $http({
           method: 'POST',
           url: '/application/index/create-task',
           data: $.param($scope.formData),
           headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         }).then(function successCallback(response) {
-            var id = response.data.id;
-            
-            if ($scope.formData.id == '') {
+            if ($scope.formData.id == undefined) {
+                // add new task to list
                 $scope.tasks = $scope.tasks.concat(
                     [[response.data.id, response.data.description, response.data.date]]
                 );
             } else {
+                // update existing task data
                 angular.forEach($scope.tasks, function(value, key) {
-                    if (value[0] == id) {
+                    if (value[0] == response.data.id) {
                         value[1] = response.data.description;
                         value[2] = response.data.date;
                     }
                 });
             }
 
+            // reset form data
             $scope.formData.description = '';
             $scope.formData.date = '';
             $scope.formData.id = '';
             $('#save-task-hdr').text('Add New Task');
             $('#create-task button').text('Create Task');
         }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
         });
     }
 
+    /*
+     * Load edit task form data
+     */
     $scope.loadEditForm = function(index) {
         $scope.formData.id = $scope.tasks[index][0];
         $scope.formData.description = $scope.tasks[index][1];
