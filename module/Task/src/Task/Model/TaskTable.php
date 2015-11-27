075703251eea52fad424b69f4767aa605254f94c
Trevor Coehoorn <tcoehoorn@gmail.com>
2015-11-27 09:10:05 -0700

    Documented task table class

--- module/Task/src/Task/Model/TaskTable.php
+++ module/Task/src/Task/Model/TaskTable.php
@@ -3,21 +3,46 @@ namespace Task\Model;
 
 use Zend\Db\TableGateway\TableGateway;
 
+/**
+ * Task table model
+ *
+ */
 class TaskTable
 {
+    /** @var TableGateway table gateway */
     protected $tableGateway;
 
+    /**
+     * Initialize class
+     *
+     * @param TableGateway $tableGateway task table gateway
+     *
+     * @return void
+     */
     public function __construct(TableGateway $tableGateway)
     {
         $this->tableGateway = $tableGateway;
     }
 
+
+    /**
+     * Get all tasks
+     *
+     * @return Zend\Db\ResultSet all tasks
+     */
     public function fetchAll()
     {
         $resultSet = $this->tableGateway->select();
         return $resultSet;
     }
 
+    /**
+     * Get task
+     *
+     * @param int $id task id
+     *
+     * @return Task\Model\Task task asssociated with id
+     */
     public function getTask($id)
     {
         $id  = (int) $id;
@@ -29,6 +54,13 @@ class TaskTable
         return $row;
     }
 
+    /**
+     * Save task
+     *
+     * @param Task $task task
+     *
+     * @return void
+     */
     public function saveTask(Task $task)
     {
         $data = array(
@@ -48,6 +80,11 @@ class TaskTable
         }
     }
 
+    /**
+     * Delete task
+     *
+     * @param int $id task id
+     */
     public function deleteTask($id)
     {
         $this->tableGateway->delete(array('id' => (int) $id));
